ddf9144326351992b31e8ebd1f5dc33e0dd6269b
Gerry <bilek.gerhard@gmx.net>
2019-05-23 17:19:11 +0200

    Masterfile erstellt

---
+++ Vorlage1.R
@@ -0,0 +1,40 @@
+library(shiny)
+library(utils)
+library(gridExtra)
+
+
+#s <- swiss
+#snames <- colnames(s)
+
+ui <- fluidPage(
+  navbarPage(title = 'perffizienz - wrong on so many levels',
+    tabPanel('swiss',
+             sidebarLayout
+              (
+               sidebarPanel(
+                  selectInput("dataset", "pick your data", choices = c("Education", "Catholic", "Fertility"))
+              )
+              ,
+               
+            mainPanel(
+              verbatimTextOutput("summary")
+              )
+             ))))
+
+  
+server <- function(input, output){
+  datasetInput <- reactive({
+    switch(input$dataset,
+           "Education" = swiss$Education,
+           "Catholic" = swiss$Catholic,
+           "Fertility" = swiss$Fertility
+           )
+  })  
+  
+  output$summary <- renderPrint({
+    dataset <- datasetInput()
+    summary(dataset)
+  })
+  }
+
+shinyApp(ui = ui, server = server)
