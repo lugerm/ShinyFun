b9f6542208d9c5ee4260b1a94a8e2f412b1d5972
Martina Luger <martinaluger@gmx.net>
2019-06-05 09:35:10 +0200

    added reactive boxplot with hard coded fertility boxplot -- we need to change it to make it dynamic with feature

--- Vorlage1.R
+++ Vorlage1.R
@@ -1,11 +1,12 @@
 library(shiny)
 library(utils)
 library(gridExtra)
+library(ggplot2) 
 
 swiss <- swiss[,-3]    # remove "Examination" from Dataset
 #snames <- colnames(s)
 
-##definition for the scatterplo
+##definition for scatterplot
 #------------------------------------------------------------------------------------------------
 panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
 {
@@ -21,52 +22,52 @@ panel.cor <- function(x, y, digits = 2, prefix = "", cex.cor, ...)
 
 ui <- fluidPage(
   navbarPage(title = 'Swiss Data',
-    #tabPanel('swiss raw data', "Mein Text"),
-    tabPanel('Exploration', tags$h3("Data Exploration: Distribution of Swiss datasets "), #tableOutput("rawdata_swiss"),
-             sidebarLayout
-              (
-                
-                sidebarPanel
-                (
-                  radioButtons("dataset", "Select a Dataset", choices = c("Fertility", "Agriculture", "Education", "Catholic", "Infant.Mortality" ))
-                  #selectInput("dataset", "Pick a variable", choices = c("Fertility", "Agriculture", "Education", "Catholic", "Infant.Mortality" )),
-                  
-                ),
-                
-              mainPanel(tags$h4("Data and Visualizations:"),
-                  #tableOutput("rawdata_swiss"),
-                  #verbatimTextOutput("summary"),
-                  #plotOutput("hist"),
-                  #plotOutput("boxplot"),
-                  tabsetPanel(
-                    tabPanel("All data", tableOutput("rawdata_swiss") ),
-                    tabPanel("Summary", verbatimTextOutput("summary") ),
-                    tabPanel("Histogram & Boxplot", plotOutput("hist"), plotOutput("boxplot")),
-                    tabPanel("QQ-Plot", plotOutput("qqplot")),
-                    tabPanel("Scatterplot", plotOutput("scatter"))
-                  )
-                )
-              )),
-    tabPanel('Correlation', "TEXT"),
-    tabPanel('Linear Model', tags$h3("Enter your dependent and independent variables")),
-            sidebarLayout(
-              sidebarPanel(
-                selectInput("regressand", "Dependent Variable", choices = c("Fertility", "Agriculture", "Education", "Catholic", "Infant.Mortality" )),
-                selectInput("regressor1", "Independent Variable 1", choices = c("Fertility", "Agriculture", "Education", "Catholic", "Infant.Mortality" )),
-                selectInput("regressor2", "Independent Variable 2", choices = c("Fertility", "Agriculture", "Education", "Catholic", "Infant.Mortality" )),
-                selectInput("regressor3", "Independent Variable 3", choices = c("Fertility", "Agriculture", "Education", "Catholic", "Infant.Mortality" ))
-                
-              ),
-              mainPanel(tags$h4("Modelle:"),
-                        actionButton("analysis","Analyze!"),
-                        verbatimTextOutput("stepmodel"),
-                        verbatimTextOutput("modelFormula"),
-                        verbatimTextOutput("modelSummary")
-              )
-            )
-    ))
+             #tabPanel('swiss raw data', "Main Text"),
+             tabPanel('Exploration', tags$h3("Data Exploration: Distribution of Swiss datasets "), #tableOutput("rawdata_swiss"),
+                      sidebarLayout
+                      (
+                        
+                        sidebarPanel
+                        (
+                          radioButtons("dataset", "Select a Dataset", choices = c("Fertility", "Agriculture", "Education", "Catholic", "Infant.Mortality" ))
+                          #selectInput("dataset", "Pick a variable", choices = c("Fertility", "Agriculture", "Education", "Catholic", "Infant.Mortality" )),
+                          
+                        ),
+                        
+                        mainPanel(tags$h4("Data and Visualizations:"),
+                                  #tableOutput("rawdata_swiss"),
+                                  #verbatimTextOutput("summary"),
+                                  #plotOutput("hist"),
+                                  #plotOutput("boxplot"),
+                                  tabsetPanel(
+                                    tabPanel("All data", tableOutput("rawdata_swiss") ),
+                                    tabPanel("Summary", verbatimTextOutput("summary") ),
+                                    tabPanel("Histogram & Boxplot", plotOutput("hist"), plotOutput(outputId = "boxplot", brush = brushOpts(direction = "y", id= "plot_brush_"))),
+                                    tabPanel("QQ-Plot", plotOutput("qqplot")),
+                                    tabPanel("Scatterplot", plotOutput("scatter"))
+                                  )
+                        )
+                      )),
+             tabPanel('Correlation', "TEXT"),
+             tabPanel('Linear Model', tags$h3("Enter your dependent and independent variables")),
+             sidebarLayout(
+               sidebarPanel(
+                 selectInput("regressand", "Dependent Variable", choices = c("Fertility", "Agriculture", "Education", "Catholic", "Infant.Mortality" )),
+                 selectInput("regressor1", "Independent Variable 1", choices = c("Fertility", "Agriculture", "Education", "Catholic", "Infant.Mortality" )),
+                 selectInput("regressor2", "Independent Variable 2", choices = c("Fertility", "Agriculture", "Education", "Catholic", "Infant.Mortality" )),
+                 selectInput("regressor3", "Independent Variable 3", choices = c("Fertility", "Agriculture", "Education", "Catholic", "Infant.Mortality" ))
+                 
+               ),
+               mainPanel(tags$h4("Modelle:"),
+                         actionButton("analysis","Analyze!"),
+                         verbatimTextOutput("stepmodel"),
+                         verbatimTextOutput("modelFormula"),
+                         verbatimTextOutput("modelSummary")
+               )
+             )
+  ))
 
-  
+
 server <- function(input, output){
   datasetInput <- reactive({
     switch(input$dataset,
@@ -77,12 +78,22 @@ server <- function(input, output){
            "Catholic" = swiss$Catholic,
            "Fertility" = swiss$Fertility,
            "Infant.Mortality" = swiss$Infant.Mortality
-           )
+    )
   })
+  
+  #--------Definition for Boxplot with interactive area 
+  # dinput = input$dataset
+  # dinput$Fertility = as.factor(dinput$Fertility)
+  
+  rds <- reactiveValues(data=swiss)
+  
+  #------------------------------------------------
+  
+  
   output$rawdata_swiss <- renderTable({
     dataset <- swiss
     dataset})  # head(dataset)
-   
+  
   output$summary <- renderPrint({
     dataset <- datasetInput()
     summary(dataset)})
@@ -91,9 +102,26 @@ server <- function(input, output){
     dataset <- datasetInput()
     hist(dataset)})
   
+  #---interactive boxplot---somehow we need to take feature into the ggplot- for now it is hard coded with only fertility--------------------------------------------------
   output$boxplot <- renderPlot({
-    dataset <- datasetInput()
-    boxplot(dataset)})
+    feature <- switch(input$dataset,
+                      "Fertility" = swiss$Fertility,
+                      "Agriculture" = swiss$Agriculture,
+                      "Examination" = swiss$Examination,
+                      "Education" = swiss$Education,
+                      "Catholic" = swiss$Catholic,
+                      "Fertility" = swiss$Fertility,
+                      "Infant.Mortality" = swiss$Infant.Mortality
+    )
+    
+    ggplot(rds$data, aes(y = Fertility)) + geom_boxplot(outlier.colour = "red") +guides(color=guide_legend(),size=guide_legend())
+  })
+  #observe function to make the plot reactive 
+  observe({
+    df = brushedPoints(rds$data, brush = input$plot_brush_, allRows = TRUE) 
+    rds$data = df[df$selected_== FALSE,]
+  })
+  #--------------------------------------------------------------------------------
   
   output$qqplot <- renderPlot({
     dataset <- datasetInput()
